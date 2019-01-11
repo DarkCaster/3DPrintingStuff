95abf0fc883550c70d2da8f06764b0c43d1c7e50
DarkCaster <dark.caster@outlook.com>
2019-01-11 19:29:40 +0300

    RepRap_Huxley_CustomParts: save_timestamps.sh script updated

--- RepRap_Huxley_CustomParts/save_timestamps.sh
+++ RepRap_Huxley_CustomParts/save_timestamps.sh
@@ -8,6 +8,6 @@ echo -n "" > "timestamps.txt"
 while IFS= read -r file; do
   stat --printf="%y|" "$file" >> "timestamps.txt"
   echo "$file" >> "timestamps.txt"
-done < <(find . -type f -not -name timestamps.txt -not -name save_timestamps.sh -not -name restore_timestamps.sh -not -name *.fcstd1 -not -name *.FCStd1 | sort)
+done < <(find . -type f -not -name timestamps.txt -not -name save_timestamps.sh -not -name restore_timestamps.sh -not -name "*.fcstd1" -not -name "*.FCStd1" | sort)
 
 echo "timestamps saved!"
