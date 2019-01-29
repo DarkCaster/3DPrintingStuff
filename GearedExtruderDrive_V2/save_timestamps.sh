39ac81a2d70206b6a17a99db96d79661995a429e
DarkCaster <dark.caster@outlook.com>
2019-01-30 00:02:45 +0300

    GearedExtruderDrive_V2: added save_timestamps.sh and restore_timestamps.sh scripts

---
+++ GearedExtruderDrive_V2/save_timestamps.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+script_dir="$( cd "$( dirname "$0" )" && pwd )"
+
+cd "$script_dir"
+echo -n "" > "timestamps.txt"
+
+while IFS= read -r file; do
+  stat --printf="%y|" "$file" >> "timestamps.txt"
+  echo "$file" >> "timestamps.txt"
+done < <(find . -type f -not -name timestamps.txt -not -name save_timestamps.sh -not -name restore_timestamps.sh -not -name "*.fcstd1" -not -name "*.FCStd1" | sort)
+
+echo "timestamps saved!"
